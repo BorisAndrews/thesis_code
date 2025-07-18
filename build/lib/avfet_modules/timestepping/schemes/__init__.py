fab459bcbb0a8d62a63c1419a5375e7e070cdd9e
Boris Andrews <boris.andrews@maths.ox.ac.uk>
2025-07-18 01:14:52 +0100

    Didn't mean to add that

--- build/lib/avfet_modules/timestepping/schemes/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Imports
-'''
-from .default_initial_guess import *
-from .runge_kutta import *
-from .collocation_runge_kutta import *
-from .weighted_collocation import *
-from .hamiltonian import *
-from .integrable import *
--- build/lib/avfet_modules/timestepping/schemes/default_initial_guess.py
+++
@@ -1,6 +0,0 @@
-'''
-Default initial guess for implicit schemes
-'''
-def default_initial_guess(x_start, dt, f):
-    from .runge_kutta import rk4
-    return rk4(x_start, dt, f)
